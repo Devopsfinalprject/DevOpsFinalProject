5f5a3bf85fff03d42079cf3a5089bc335790e6f6
Saw <ehkalanephaung96@gmail.com>
2023-04-01 15:39:09 +0700

    create remove order function

--- model/cart.js
+++ model/cart.js
@@ -30,5 +30,11 @@ module.exports = function Cart(oldCart) {
       this.totalQty++;
       this.totalPrice += this.items[id].item.price;
     };
+
+    this.removeItem = function (id) {
+      this.totalQty -= this.items[id].qty;
+      this.totalPrice -= this.items[id].price;
+      delete this.items[id];
+    };
   };
 };
