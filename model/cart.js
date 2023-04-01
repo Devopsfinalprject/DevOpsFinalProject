f4daac811f19c80d45c5ac500d510dfff9284e8c
Saw <ehkalanephaung96@gmail.com>
2023-04-01 15:37:36 +0700

    Create reduce or descrease  order function

--- model/cart.js
+++ model/cart.js
@@ -12,5 +12,16 @@ module.exports = function Cart(oldCart) {
     storeItem.price = storeItem.item.price * storeItem.qty;
     this.totalQty++;
     this.totalPrice += storeItem.item.price;
+
+    this.reduceByOne = function (id) {
+      this.items[id].qty--;
+      this.items[id].price -= this.items[id].item.price;
+      this.totalQty--;
+      this.totalPrice -= this.items[id].item.price;
+
+      if (this.items[id].qty <= 0) {
+        delete this.items[id];
+      }
+    };
   };
 };
